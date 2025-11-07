a19c0bf869ceaaf2535f34c9815a36cfbb6b0e64
Prajapati Yash <29yashprajapat@gmail.com>
2025-11-08 01:58:21 +0530

    Update EditorPanel.jsx

--- client/src/components/EditorPanel.jsx
+++ client/src/components/EditorPanel.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 function EditorPanel({ code, setCode }) {
   return (
     <textarea
       className="editor-panel"
       value={code}
       onChange={(e) => setCode(e.target.value)}
-      placeholder="Paste or write your code here..."
+      placeholder="Enter your code or text here..."
     />
   );
 }
